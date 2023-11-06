97afa5438689774ec37ca6363127cea7cbc7a776
SUBHIN-TM <subhintm2018@gmail.com>
2023-11-06 20:09:24 +0530

    validate

--- server.js
+++ server.js
@@ -213,7 +213,7 @@ const server = http.createServer((req, res) => {
 
 
 server.listen(3000, () => {
-    console.log("server is running on port 30000");
+    console.log("server is running on port 3000");
 })
 
 
